634950a4e8a14a0fd33ea8008f87b3628706a609
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 <morara1920@gmail.com>
2024-04-25 05:01:14 +0300

    files update

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,43 @@
+# Puppet to make changes to our configuration file
+# Just as in the previous configuration file task,
+# we’d like you to set up your client SSH configuration file
+# so that you can connect to a server without typing a password.
+# Requirements:
+# Your SSH client configuration must be configured to use the private key ~/.ssh/school
+# Your SSH client configuration must be configured to refuse to authenticate using a password
+#
+#
+
+include stdlib
+
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
+}
+#_________________________________________________________________________#
+#
+
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
+}
+#_________________________________________________________________________________#
+#
+
+# Regex match explanation
+#
+# ^       beginning of the line
+# [#]*  atleast one hash character
+# [\s]*  zero or more white space characters
+# (?i)IdentityFile case insensitive "IdentityFile"
+# (?i)PasswordAuthentication case insensitive "PasswordAuthentication"
+# [\s]+ at least one whitespace character
+# ~/.ssh/id_rsa The ssh private key file path we want to replace
+# (yes|no) with the value "yes" or the value "no"
+# $      end of the line 
