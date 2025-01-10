4999eec6d88c95d9960df7134facd4fb6bdfc9ea
Tom Morara <morara1920@gmail.com>
2025-01-10 20:42:32 +0300

    done

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Puppet manifest to create a file at /tmp/school
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
+
