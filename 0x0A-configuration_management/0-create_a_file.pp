d54fcfbd054ed3006a7391f6393ece5aad403d4b
tommorara <119826192+tommorara@users.noreply.github.com>
2025-01-10 20:21:15 +0300

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# Puppet manifest to create a file at /tmp/school
-file { '/tmp/school':
-  ensure  => 'file',
-  content => 'I love Puppet',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0744',
-}
-
