f214375f207d85c6ff7f6858a9c9939c0551d697
Tom Morara <morara1920@gmail.com>
2025-01-11 03:50:03 +0300

    revised

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# Puppet manifest to install Flask version 2.1.0 using pip3
+# Manifest that installs Flask version 2.1.0
+
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
+  ensure     => '2.1.0',
+    provider => 'pip3',
+    }
+
+package {'werkzeug':
+  ensure     => '2.1.1',
+    provider => 'pip3',
+    }
 
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
-# Puppet manifest to kill a process named 'killmenow'
-exec { 'kill_process':
-  command     => 'pkill -f killmenow || true',
-  path        => '/usr/bin:/bin',
-  refreshonly => false,
-}
+# Ensure the killmenow process is killed
+
+exec { 'killmenow':
+  command => '/usr/bin/pkill killmenow',
+    path  => ['/usr/bin', '/bin'], # Specify the path to pkill if needed
+  }
 
