de6234a4afb867694041cb0d6914501e5f3e9189
Tom Morara <morara1920@gmail.com>
2025-02-26 04:50:48 +0300

    pycodestyele

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -34,6 +34,8 @@
 """
 import requests
 def count_words(subreddit, word_list, word_count=[], page_after=None):
+    
+    
     """
     Prints the count of the given words present in the title of the
     subreddit's hottest articles.
